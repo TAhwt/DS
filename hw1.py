2689cc9f022eb1440e890e157be8f513d1f23a9e
TAhwt <tsikulev-alexandr@ya.ru>
2020-04-25 16:05:16 +0300

    L1_E1
    Урок 1, задание 1

---
+++ hw1.py
@@ -0,0 +1,16 @@
+#Поработайте с переменными, создайте несколько, выведите на экран, запросите у
+#пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
+
+x = 5
+y = 7
+z = x + y
+print('Первая выведенная переменная: z = x + y = ', z)
+
+my_first_number = input('Введите число1: ')
+my_second_number = input('Введите число2: ')
+my_first_string = input('Введите строку1: ')
+my_second_string = input('Введите строку2: ')
+print(my_first_number)
+print(my_second_number)
+print(my_first_string)
+print(my_second_string)
